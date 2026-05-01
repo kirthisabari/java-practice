dfe52f219fddab59ea149cc18f0ee6973b71e593
kirthisabari <130929544+kirthisabari@users.noreply.github.com>
2026-05-02 01:47:24 +0530

    Update main.java

--- polymorphism/main.java
+++ polymorphism/main.java
@@ -1,1 +1,33 @@
+class Employee{
+    void salary(){
+        System.out.println("Salary is "+20000);
+      }
+}
+ class Manager extends Employee{
+    void salary(){
+        System.out.println("Manager salary is :50000 ");
+        
+    }
 
+ }
+
+ class peon extends Employee{
+    void salary(){
+        System.out.println("Peon salary is :5000 ");
+    }
+
+    void display(){
+      System.out.println("This is Employee peon class");
+    }
+ }
+ 
+ public class main{
+    public static void main(String[] args){
+      Employee e1=new Manager();
+      Employee e2=new peon();
+      e1.salary();
+      e2.salary();
+      
+    }
+ }
+ 
